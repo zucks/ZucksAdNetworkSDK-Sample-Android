5fdb61be52f52bb05b023ec07f37a95d113a22aa
Shigeki Yamato <heki1224@gmail.com>
2016-05-18 13:29:26 +0900

    Add sample banner.

--- app/src/main/java/net/zucks/zucksadnetworksdk/sample/android/BannerCallbackActivity.java
+++ app/src/main/java/net/zucks/zucksadnetworksdk/sample/android/BannerCallbackActivity.java
@@ -45,5 +45,31 @@ public class BannerCallbackActivity extends AppCompatActivity {
 
         layout.addView(adBanner);
         adBanner.load();
+
+        // サイズアジャスト
+        AdBanner sizeAdjustBanner = new AdBanner(this, "_833b45aa06", new AdBannerListener() {
+            @Override
+            public void onReceiveAd(AdBanner adBanner) {
+                // 広告情報のロードが成功した場合
+                Toast.makeText(BannerCallbackActivity.this, "onReceiveAd", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTapAd(AdBanner adBanner) {
+                // 広告がタップされた場合
+                Toast.makeText(BannerCallbackActivity.this, "onTapAd", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(AdBanner adBanner, Exception e) {
+                // 広告情報のロードに失敗した場合
+                Toast.makeText(BannerCallbackActivity.this, "onFailure", Toast.LENGTH_SHORT).show();
+            }
+        }, true);
+        RelativeLayout.LayoutParams params2 = (RelativeLayout.LayoutParams) sizeAdjustBanner.getLayoutParams();
+        params2.addRule(RelativeLayout.CENTER_VERTICAL);
+        params2.addRule(RelativeLayout.CENTER_IN_PARENT);
+        layout.addView(sizeAdjustBanner);
+        sizeAdjustBanner.load();
     }
 }

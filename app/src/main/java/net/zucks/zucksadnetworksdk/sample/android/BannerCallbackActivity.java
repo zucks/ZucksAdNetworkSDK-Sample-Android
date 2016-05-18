18a5bf65012fa90f73d2ca21cd23b9cb7e95146c
Shigeki Yamato <heki1224@gmail.com>
2016-05-18 14:43:43 +0900

    Add sample banner.

--- app/src/main/java/net/zucks/zucksadnetworksdk/sample/android/BannerCallbackActivity.java
+++ app/src/main/java/net/zucks/zucksadnetworksdk/sample/android/BannerCallbackActivity.java
@@ -2,7 +2,9 @@ package net.zucks.zucksadnetworksdk.sample.android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import net.zucks.listener.AdBannerListener;
@@ -16,6 +18,16 @@ public class BannerCallbackActivity extends AppCompatActivity {
         setContentView(R.layout.activity_banner_callback);
         RelativeLayout layout = (RelativeLayout) findViewById(R.id.layout_banner);
 
+        // 通常バナー テキスト
+        TextView normal = new TextView(this);
+        normal.setId(R.id.text_normal);
+        RelativeLayout.LayoutParams textParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        textParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        normal.setLayoutParams(textParams);
+        normal.setText("通常バナー");
+        layout.addView(normal);
+
+        // バナー本体
         // コードで記述する場合（コールバックあり）
         // 管理画面から取得したFrame IDを設定してください
         AdBanner adBanner = new AdBanner(this, "_833b45aa06", new AdBannerListener() {
@@ -40,13 +52,23 @@ public class BannerCallbackActivity extends AppCompatActivity {
 
         // ページ上部中央に寄せて配置する
         RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) adBanner.getLayoutParams();
-        params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        params.addRule(RelativeLayout.BELOW, R.id.text_normal);
         params.addRule(RelativeLayout.CENTER_IN_PARENT);
 
         layout.addView(adBanner);
         adBanner.load();
 
         // サイズアジャスト
+        // サイズアジャスト テキスト
+        TextView sizeAdjust = new TextView(this);
+        sizeAdjust.setId(R.id.text_size_adjust);
+        RelativeLayout.LayoutParams textParams2 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        textParams2.addRule(RelativeLayout.CENTER_VERTICAL);
+        sizeAdjust.setLayoutParams(textParams2);
+        sizeAdjust.setText("サイズアジャストバナー");
+        layout.addView(sizeAdjust);
+
+        // バナー本体
         AdBanner sizeAdjustBanner = new AdBanner(this, "_833b45aa06", new AdBannerListener() {
             @Override
             public void onReceiveAd(AdBanner adBanner) {
@@ -67,7 +89,7 @@ public class BannerCallbackActivity extends AppCompatActivity {
             }
         }, true);
         RelativeLayout.LayoutParams params2 = (RelativeLayout.LayoutParams) sizeAdjustBanner.getLayoutParams();
-        params2.addRule(RelativeLayout.CENTER_VERTICAL);
+        params2.addRule(RelativeLayout.BELOW, R.id.text_size_adjust);
         params2.addRule(RelativeLayout.CENTER_IN_PARENT);
         layout.addView(sizeAdjustBanner);
         sizeAdjustBanner.load();
--- app/src/main/java/net/zucks/zucksadnetworksdk/sample/android/BannerNoCallbackActivity.java
+++ app/src/main/java/net/zucks/zucksadnetworksdk/sample/android/BannerNoCallbackActivity.java
@@ -2,7 +2,9 @@ package net.zucks.zucksadnetworksdk.sample.android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import net.zucks.view.AdBanner;
 
@@ -14,22 +16,41 @@ public class BannerNoCallbackActivity extends AppCompatActivity {
         setContentView(R.layout.activity_banner_no_callback);
         RelativeLayout layout = (RelativeLayout) findViewById(R.id.layout_banner);
 
+        // 通常バナー テキスト
+        TextView normal = new TextView(this);
+        normal.setId(R.id.text_normal);
+        RelativeLayout.LayoutParams textParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        textParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        normal.setLayoutParams(textParams);
+        normal.setText("通常バナー");
+        layout.addView(normal);
+
         // コードで記述する場合（コールバックなし）
         // 管理画面から取得したFrame IDを設定してください
         AdBanner banner = new AdBanner(this, "_833b45aa06");
 
         // ページ上部中央に寄せて配置する
         RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) banner.getLayoutParams();
-        params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        params.addRule(RelativeLayout.BELOW, R.id.text_normal);
         params.addRule(RelativeLayout.CENTER_IN_PARENT);
 
         layout.addView(banner);
         banner.load();
 
         // サイズアジャスト
+        // サイズアジャスト
+        // サイズアジャスト テキスト
+        TextView sizeAdjust = new TextView(this);
+        sizeAdjust.setId(R.id.text_size_adjust);
+        RelativeLayout.LayoutParams textParams2 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        textParams2.addRule(RelativeLayout.CENTER_VERTICAL);
+        sizeAdjust.setLayoutParams(textParams2);
+        sizeAdjust.setText("サイズアジャストバナー");
+        layout.addView(sizeAdjust);
+
         AdBanner sizeAdjustBanner = new AdBanner(this, "_833b45aa06", true);
         RelativeLayout.LayoutParams params2 = (RelativeLayout.LayoutParams) sizeAdjustBanner.getLayoutParams();
-        params2.addRule(RelativeLayout.CENTER_VERTICAL);
+        params2.addRule(RelativeLayout.BELOW, R.id.text_size_adjust);
         params2.addRule(RelativeLayout.CENTER_IN_PARENT);
         layout.addView(sizeAdjustBanner);
         sizeAdjustBanner.load();
